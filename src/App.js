25e0932bbf2703f9f68e81eb9d2f7077f287a67f
eunsunShin <unusual0218@gmail.com>
2022-03-09 19:53:42 +0900

    Add: CRA 기반으로 container 구현

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes, Redirect } from "react-router-dom";
+import AppHeader from "./AppHeader";
+import MicroFrontend from "./MicroFrontend";
+import About from "./About";
+import "./App.css";
+
+const {
+  REACT_APP_BROWSE_HOST: browseHost,
+  REACT_APP_RESTAURANT_HOST: restaurantHost,
+} = process.env;
+
+let numRestaurants = 0;
+fetch(`${process.env.REACT_APP_CONTENT_HOST}/restaurants.json`)
+  .then((res) => res.json())
+  .then((restaurants) => {
+    numRestaurants = restaurants.length;
+  });
+
+const getRandomRestaurantId = () =>
+  Math.floor(Math.random() * numRestaurants) + 1;
+
+const Browse = ({ history }) => {
+  return <MicroFrontend history={history} host={browseHost} name="Browse" />;
+};
+
+const Restaurant = ({ history }) => (
+  <MicroFrontend history={history} host={restaurantHost} name="Restaurant" />
+);
+
+// const Random = () => <Redirect to={`/restaurant/${getRandomRestaurantId()}`} />;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      
-      </header>
-    </div>
+    <BrowserRouter>
+      <AppHeader />
+      <Routes>
+        <Route path="/" element={<Browse />} />
+        <Route path="/restaurant/:id" element={<Restaurant />} />
+        {/* <Route path="/random" element={<Random />} /> */}
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/MicroFrontend.js
@@ -0,0 +1,37 @@
+import React, { useEffect } from "react";
+
+export default function MicroFrontend(props) {
+  useEffect(() => {
+    const { name, host } = props;
+    const scriptId = `micro-frontend-script-${name}`;
+
+    if (document.getElementById(scriptId)) {
+      renderMicroFrontend();
+      return;
+    }
+
+    fetch(`${host}/asset-manifest.json`)
+    .then(res => res.json())
+    .then(manifest => {
+      const script = document.createElement('script');
+      script.id = scriptId;
+      script.crossOrigin = '';
+      script.src = `${host}${manifest['main.js']}`;
+      script.onload = this.renderMicroFrontend;
+      document.head.appendChild(script);
+    });
+
+    return () => {
+      window[`unmount${name}`](`${name}-container`);
+    }
+  })
+
+  const renderMicroFrontend = () => {
+    const { name, window, history } = props;
+    window[`render${name}`](`${name}-container`, history);
+  }
+
+  return (
+    <main id={`${props.name}-container`} />
+  )
+}
